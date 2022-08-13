5fb58c0af8624e6e7bd309a24227f9a1866c003f
Ernesto Cardenas LLanes <ernesto.cardenas79@gmail.com>
2022-08-13 13:29:31 -0600

    Chapter01

---
+++ chapter01/HelloCs/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello From Top Level Program"+ args[0]);
+Console.WriteLine(Environment.OSVersion.VersionString);
---
+++ chapter01/TopLevelProgram/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello From Top Level Program");
+Console.WriteLine(Environment.OSVersion.VersionString);
